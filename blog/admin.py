6107babd95c8c7e2a2e6a550a46c0b5be6749cbd
Gibbons <lgibbons@cannonschool.org>
2023-12-04 10:14:45 -0500

    got post approval 100% working!

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Post, ThingsToApprove
+import random
 # Register your models here.
+
+funny_words = [
+    "Bumblebee", "Wobble", "Gobbledygook", "Quizzical", "Wiggly", "Snicker",
+    "Blubber", "Flabbergasted", "Lollygag", "Gobbledygook", "Noodle", "Quibble",
+    "Snollygoster", "Kerfuffle", "Doozy", "Brouhaha", "Giggly", "Gobsmacked",
+    "Whippersnapper", "Lollygag", "Quokka", "Snollygoster", "Wobble", "Dunderhead",
+    "Quizzical", "Quibble", "Higgledy-piggledy", "Bamboozle", "Noodle", "Gobbledygook",
+    "Hullabaloo", "Snicker", "Flibbertigibbet", "Malarkey", "Noodledoodle", "Wobble",
+    "Quibble", "Bumfuzzle", "Giggly", "Flummox"
+]
+
+@admin.action(description="Duplicate Blog")
+def dupe_blog(modeladmin, request, queryset):
+    for _ in range(5):
+        for obj in queryset:
+                obj.pk = None  # Set primary key to None to create a new instance
+                obj.name = random.choice(funny_words)
+                obj.approved = False
+                obj.save()
 
 class PostAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -12,6 +32,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ["name", "pub_date", "picture", "words"]
     list_filter = ["pub_date"]
     search_fields = ["name"]
+    actions = [dupe_blog]
 
 class ThingsToApproveAdmin(admin.ModelAdmin):
     fieldsets = [
--- display/admin.py
+++ display/admin.py
@@ -3,6 +3,30 @@ from .models import Club
 # Register your models here.
 from google_sheets import sheets
 from email_schedule import coolmain
+import random
+
+funny_words = [
+    "Bumblebee", "Wobble", "Gobbledygook", "Quizzical", "Wiggly", "Snicker",
+    "Blubber", "Flabbergasted", "Lollygag", "Gobbledygook", "Noodle", "Quibble",
+    "Snollygoster", "Kerfuffle", "Doozy", "Brouhaha", "Giggly", "Gobsmacked",
+    "Whippersnapper", "Lollygag", "Quokka", "Snollygoster", "Wobble", "Dunderhead",
+    "Quizzical", "Quibble", "Higgledy-piggledy", "Bamboozle", "Noodle", "Gobbledygook",
+    "Hullabaloo", "Snicker", "Flibbertigibbet", "Malarkey", "Noodledoodle", "Wobble",
+    "Quibble", "Bumfuzzle", "Giggly", "Flummox"
+]
+
+@admin.action(description="Duplicate Club")
+def dupe_club(modeladmin, request, queryset):
+    for _ in range(5):
+        for obj in queryset:
+                obj.pk = None  # Set primary key to None to create a new instance
+                obj.name = random.choice(funny_words)
+                obj.approved = False
+                obj.save()
+
+
+
+
 @admin.action(description="Reset Club Information from A Spreadsheet")
 def reset_sheets(modeladmin, request, queryset):
     link = queryset.first().sheet_link
@@ -35,7 +59,7 @@ class ClubAdmin(admin.ModelAdmin):
     list_display = ["name", "leaders", "emails", "description"]
     #list_filter = ["name"]
     search_fields = ["name"]
-    actions = [reset_sheets, remove_from_sheet, send_email_to_me]
+    actions = [reset_sheets, remove_from_sheet, send_email_to_me, dupe_club]
     
 
 admin.site.register(Club, ClubAdmin)
